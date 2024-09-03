164dd0e04130fa22fea688157454b1da95dae794
Abebe123000 <takashi123000@yahoo.co.jp>
2024-09-03 19:20:31 +0900

    -#7 format:ci と tsc を追加

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 import appHtml from "./app.html?raw";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = appHtml;
+(<HTMLDivElement>document.querySelector("#app")).innerHTML = appHtml;
 
 document.getElementById("submit")?.addEventListener("click", () => {
 	console.log("hoge");
